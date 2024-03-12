82cb736e3bc062888f0112f472b64115dc03c0fb
0_o.Mihai <mustucmihai78@gmail.com>
2024-03-12 10:33:42 +0200

    add solvingTask method

--- ASP.NET/1.Assignment Version Control and Git/TestApp/Program.cs
+++ ASP.NET/1.Assignment Version Control and Git/TestApp/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello from Developer 1");
-Console.WriteLine("Hello from Developer 2!");
+
+SolveingTask();
+static void SolveingTask()
+{
+    Console.WriteLine("I will finish the task in 4 hours");
+}
